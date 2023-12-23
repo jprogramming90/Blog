1511de49df101c7e5b60840444d98ee8cc76e56c
jprogramming90 <129398052+jprogramming90@users.noreply.github.com>
2023-12-23 18:00:10 +0800

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,219 @@
+function jazzyHa() {
+    document.getElementById("jazzyHa").style.backgroundColor="yellow";
+    document.getElementById("jazzyS").style.backgroundColor="chocolate";
+    document.getElementById("jazzyHe").style.backgroundColor="chocolate";
+    document.getElementById("jazzyL").style.backgroundColor="chocolate";
+    document.getElementById("countjazzyHa").innerHTML="1";
+    document.getElementById("countjazzyS").innerHTML="";
+    document.getElementById("countjazzyHe").innerHTML="";
+    document.getElementById("countjazzyL").innerHTML="";
+}
+
+function jazzyS() {
+    document.getElementById("jazzyHa").style.backgroundColor="chocolate";
+    document.getElementById("jazzyS").style.backgroundColor="yellow";
+    document.getElementById("jazzyHe").style.backgroundColor="chocolate";
+    document.getElementById("jazzyL").style.backgroundColor="chocolate";
+    document.getElementById("countjazzyHa").innerHTML="";
+    document.getElementById("countjazzyS").innerHTML="1";
+    document.getElementById("countjazzyHe").innerHTML="";
+    document.getElementById("countjazzyL").innerHTML="";
+}
+
+function jazzyHe() {
+    document.getElementById("jazzyHa").style.backgroundColor="chocolate";
+    document.getElementById("jazzyS").style.backgroundColor="chocolate";
+    document.getElementById("jazzyHe").style.backgroundColor="red";
+    document.getElementById("jazzyL").style.backgroundColor="chocolate";
+    document.getElementById("countjazzyHa").innerHTML="";
+    document.getElementById("countjazzyS").innerHTML="";
+    document.getElementById("countjazzyHe").innerHTML="1";
+    document.getElementById("countjazzyL").innerHTML="";
+}
+
+function jazzyL() {
+    document.getElementById("jazzyHa").style.backgroundColor="chocolate";
+    document.getElementById("jazzyS").style.backgroundColor="chocolate";
+    document.getElementById("jazzyHe").style.backgroundColor="chocolate";
+    document.getElementById("jazzyL").style.backgroundColor="blue";
+    document.getElementById("countjazzyHa").innerHTML="";
+    document.getElementById("countjazzyS").innerHTML="";
+    document.getElementById("countjazzyHe").innerHTML="";
+    document.getElementById("countjazzyL").innerHTML="1";
+}
+
+function robertHa() {
+    document.getElementById("robertHa").style.backgroundColor="yellow";
+    document.getElementById("robertS").style.backgroundColor="chocolate";
+    document.getElementById("robertHe").style.backgroundColor="chocolate";
+    document.getElementById("robertL").style.backgroundColor="chocolate";
+    document.getElementById("countrobertHa").innerHTML="1";
+    document.getElementById("countrobertS").innerHTML="";
+    document.getElementById("countrobertHe").innerHTML="";
+    document.getElementById("countrobertL").innerHTML="";
+}
+
+function robertS() {
+    document.getElementById("robertHa").style.backgroundColor="chocolate";
+    document.getElementById("robertS").style.backgroundColor="yellow";
+    document.getElementById("robertHe").style.backgroundColor="chocolate";
+    document.getElementById("robertL").style.backgroundColor="chocolate";
+    document.getElementById("countrobertHa").innerHTML="";
+    document.getElementById("countrobertS").innerHTML="1";
+    document.getElementById("countrobertHe").innerHTML="";
+    document.getElementById("countrobertL").innerHTML="";
+}
+
+function robertHe() {
+    document.getElementById("robertHa").style.backgroundColor="chocolate";
+    document.getElementById("robertS").style.backgroundColor="chocolate";
+    document.getElementById("robertHe").style.backgroundColor="red";
+    document.getElementById("robertL").style.backgroundColor="chocolate";
+    document.getElementById("countrobertHa").innerHTML="";
+    document.getElementById("countrobertS").innerHTML="";
+    document.getElementById("countrobertHe").innerHTML="1";
+    document.getElementById("countrobertL").innerHTML="";
+}
+
+function robertL() {
+    document.getElementById("robertHa").style.backgroundColor="chocolate";
+    document.getElementById("robertS").style.backgroundColor="chocolate";
+    document.getElementById("robertHe").style.backgroundColor="chocolate";
+    document.getElementById("robertL").style.backgroundColor="blue";
+    document.getElementById("countrobertHa").innerHTML="";
+    document.getElementById("countrobertS").innerHTML="";
+    document.getElementById("countrobertHe").innerHTML="";
+    document.getElementById("countrobertL").innerHTML="1";
+}
+
+function randyHa() {
+    document.getElementById("randyHa").style.backgroundColor="yellow";
+    document.getElementById("randyS").style.backgroundColor="chocolate";
+    document.getElementById("randyHe").style.backgroundColor="chocolate";
+    document.getElementById("randyL").style.backgroundColor="chocolate";
+    document.getElementById("countrandyHa").innerHTML="1";
+    document.getElementById("countrandyS").innerHTML="";
+    document.getElementById("countrandyHe").innerHTML="";
+    document.getElementById("countrandyL").innerHTML="";
+}
+
+function randyS() {
+    document.getElementById("randyHa").style.backgroundColor="chocolate";
+    document.getElementById("randyS").style.backgroundColor="yellow";
+    document.getElementById("randyHe").style.backgroundColor="chocolate";
+    document.getElementById("randyL").style.backgroundColor="chocolate";
+    document.getElementById("countrandyHa").innerHTML="";
+    document.getElementById("countrandyS").innerHTML="1";
+    document.getElementById("countrandyHe").innerHTML="";
+    document.getElementById("countrandyL").innerHTML="";
+}
+
+function randyHe() {
+    document.getElementById("randyHa").style.backgroundColor="chocolate";
+    document.getElementById("randyS").style.backgroundColor="chocolate";
+    document.getElementById("randyHe").style.backgroundColor="red";
+    document.getElementById("randyL").style.backgroundColor="chocolate";
+    document.getElementById("countrandyHa").innerHTML="";
+    document.getElementById("countrandyS").innerHTML="";
+    document.getElementById("countrandyHe").innerHTML="1";
+    document.getElementById("countrandyL").innerHTML="";
+}
+
+function randyL() {
+    document.getElementById("randyHa").style.backgroundColor="chocolate";
+    document.getElementById("randyS").style.backgroundColor="chocolate";
+    document.getElementById("randyHe").style.backgroundColor="chocolate";
+    document.getElementById("randyL").style.backgroundColor="blue";
+    document.getElementById("countrandyHa").innerHTML="";
+    document.getElementById("countrandyS").innerHTML="";
+    document.getElementById("countrandyHe").innerHTML="";
+    document.getElementById("countrandyL").innerHTML="1";
+}
+
+function jeraldHa() {
+    document.getElementById("jeraldHa").style.backgroundColor="yellow";
+    document.getElementById("jeraldS").style.backgroundColor="chocolate";
+    document.getElementById("jeraldHe").style.backgroundColor="chocolate";
+    document.getElementById("jeraldL").style.backgroundColor="chocolate";
+    document.getElementById("countjeraldHa").innerHTML="1";
+    document.getElementById("countjeraldS").innerHTML="";
+    document.getElementById("countjeraldHe").innerHTML="";
+    document.getElementById("countjeraldL").innerHTML="";
+}
+
+function jeraldS() {
+    document.getElementById("jeraldHa").style.backgroundColor="chocolate";
+    document.getElementById("jeraldS").style.backgroundColor="yellow";
+    document.getElementById("jeraldHe").style.backgroundColor="chocolate";
+    document.getElementById("jeraldL").style.backgroundColor="chocolate";
+    document.getElementById("countjeraldHa").innerHTML="";
+    document.getElementById("countjeraldS").innerHTML="1";
+    document.getElementById("countjeraldHe").innerHTML="";
+    document.getElementById("countjeraldL").innerHTML="";
+}
+
+function jeraldHe() {
+    document.getElementById("jeraldHa").style.backgroundColor="chocolate";
+    document.getElementById("jeraldS").style.backgroundColor="chocolate";
+    document.getElementById("jeraldHe").style.backgroundColor="red";
+    document.getElementById("jeraldL").style.backgroundColor="chocolate";
+    document.getElementById("countjeraldHa").innerHTML="";
+    document.getElementById("countjeraldS").innerHTML="";
+    document.getElementById("countjeraldHe").innerHTML="1";
+    document.getElementById("countjeraldL").innerHTML="";
+}
+
+function jeraldL() {
+    document.getElementById("jeraldHa").style.backgroundColor="chocolate";
+    document.getElementById("jeraldS").style.backgroundColor="chocolate";
+    document.getElementById("jeraldHe").style.backgroundColor="chocolate";
+    document.getElementById("jeraldL").style.backgroundColor="blue";
+    document.getElementById("countjeraldHa").innerHTML="";
+    document.getElementById("countjeraldS").innerHTML="";
+    document.getElementById("countjeraldHe").innerHTML="";
+    document.getElementById("countjeraldL").innerHTML="1";
+}
+
+function chanHa() {
+    document.getElementById("chanHa").style.backgroundColor="yellow";
+    document.getElementById("chanS").style.backgroundColor="chocolate";
+    document.getElementById("chanHe").style.backgroundColor="chocolate";
+    document.getElementById("chanL").style.backgroundColor="chocolate";
+    document.getElementById("countchanHa").innerHTML="1";
+    document.getElementById("countchanS").innerHTML="";
+    document.getElementById("countchanHe").innerHTML="";
+    document.getElementById("countchanL").innerHTML="";
+}
+
+function chanS() {
+    document.getElementById("chanHa").style.backgroundColor="chocolate";
+    document.getElementById("chanS").style.backgroundColor="yellow";
+    document.getElementById("chanHe").style.backgroundColor="chocolate";
+    document.getElementById("chanL").style.backgroundColor="chocolate";
+    document.getElementById("countchanHa").innerHTML="";
+    document.getElementById("countchanS").innerHTML="1";
+    document.getElementById("countchanHe").innerHTML="";
+    document.getElementById("countchanL").innerHTML="";
+}
+
+function chanHe() {
+    document.getElementById("chanHa").style.backgroundColor="chocolate";
+    document.getElementById("chanS").style.backgroundColor="chocolate";
+    document.getElementById("chanHe").style.backgroundColor="red";
+    document.getElementById("chanL").style.backgroundColor="chocolate";
+    document.getElementById("countchanHa").innerHTML="";
+    document.getElementById("countchanS").innerHTML="";
+    document.getElementById("countchanHe").innerHTML="1";
+    document.getElementById("countchanL").innerHTML="";
+}
+
+function chanL() {
+    document.getElementById("chanHa").style.backgroundColor="chocolate";
+    document.getElementById("chanS").style.backgroundColor="chocolate";
+    document.getElementById("chanHe").style.backgroundColor="chocolate";
+    document.getElementById("chanL").style.backgroundColor="blue";
+    document.getElementById("countchanHa").innerHTML="";
+    document.getElementById("countchanS").innerHTML="";
+    document.getElementById("countchanHe").innerHTML="";
+    document.getElementById("countchanL").innerHTML="1";
+}
